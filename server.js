37095c45c33a39b5220a8d7335f0a5e0538bd1b0
Keve1227 <killenlillen02@gmail.com>
2019-11-24 20:44:25 +0100

    Implemented basic functionality.

---
+++ server.js
@@ -0,0 +1,39 @@
+const config = require("./config.json");
+const crypto = require("crypto");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+
+mongoose.connect(config.databaseUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const Token = mongoose.model("Token", { id: String, user: Object, created: Date });
+
+const generateTokenID = function () {
+    return crypto.randomBytes(72).toString('base64');
+}
+
+const app = express();
+app.use(cookieParser());
+
+app.get("/authorize", (req, res) => {
+    if (!req.query["origin"]) {
+        return res.status(400).send("Bad Request");
+    }
+
+    let id = generateTokenID();
+    let user = null;
+
+    try {
+        user = jwt.verify(req.cookies["session_token"], config.jwt.key, { algorithms: [ config.jwt.alg ] });
+    } catch (err) {
+        res.cookie("session_token", "", { maxAge: 0, httpOnly: true, secure: true });
+    }
+
+    Token.create({ id: id, user: user }, (_, token) => {
+        res.redirect(`${req.query["origin"]}?tokenid=${token.id}`);
+    });
+});
+
+app.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}.`);
+});
