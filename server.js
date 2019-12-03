d6d0d83f3eaed9f81c1342e1a724b7e849a1df86
Keve1227 <killenlillen02@gmail.com>
2019-12-04 00:35:09 +0100

    Added login/logout functionality.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const config = require("./config.json");
 const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const express = require("express");
+const authMiddleware = require("@tusent.io/authentication-middleware")();
 
 mongoose.connect(config.databaseUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const Token = mongoose.model("Token", { id: String, user: Object, created: Date });
+const User = mongoose.model("User", { id: String, email: String, pword_hash: String, profile: Object });
 
 const generateSSOID = function () {
     return crypto.randomBytes(72).toString('base64');
@@ -14,6 +17,16 @@ const generateSSOID = function () {
 
 const app = express();
 app.use(cookieParser());
+
+app.get("/", authMiddleware.authenticate, (req, res) => {
+    req.status(200);
+
+    if (req.user) {
+        res.sendFile("views/userpage.html", { root: __dirname });
+    } else {
+        res.sendFile("views/signin.html", { root: __dirname });
+    }
+});
 
 app.get("/authenticate", (req, res) => {
     let origin;
@@ -37,6 +50,41 @@ app.get("/authenticate", (req, res) => {
         let escapedTokenID = encodeURIComponent(token.id);
         res.redirect(`${origin}?ssoid=${escapedTokenID}`);
     });
+});
+
+app.post("/login", (req, res) => {
+    let email = req.body["email"];
+    let uncheckedPword = req.body["password"];
+
+    if (email == null || password == null) {
+        return res.status(400).send("Bad Request");
+    }
+
+    User.findOne({ email: email }, (err, userDoc) => {
+        if (err) return res.status(500).send("Internal Server Error");
+        if (userDoc == null) return res.status(403).send("Forbidden");
+
+        bcrypt.compare(uncheckedPword, userDoc.pword_hash, (err, same) => {
+            if (err) return res.status(500).send("Internal Server Error");
+            if (!same) return res.status(403).send("Forbidden");
+
+            let user = userDoc.profile;
+            let sessionToken = jwt.sign(user, config.jwt.key, { algorithm: config.jwt.alg });
+
+            res.cookie("session_token", sessionToken, { httpOnly: true, secure: true });
+            res.status(200).send("OK");
+        });
+    });
+});
+
+app.post("/logout", (_, res) => {
+    res.cookie("session_token", "", { maxAge: 0, httpOnly: true, secure: true });
+    res.status(200).send("OK");
+});
+
+app.post("/register", (_, res) => {
+    // Placeholder
+    res.status(501).send("Not Implemented");
 });
 
 app.listen(config.port, () => {
